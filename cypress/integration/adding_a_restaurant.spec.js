f7ffbc8ff3a075ad20a647518b2650229f90d05e
Josh Justice <me@codingitwrong.com>
2018-10-15 14:55:38 -0400

    Conditionally show new restaurant form

--- cypress/integration/adding_a_restaurant.spec.js
+++ cypress/integration/adding_a_restaurant.spec.js
@@ -4,6 +4,9 @@ describe('adding a restaurant', () => {
 
     cy.visit('http://localhost:1234');
 
+    cy.get('[data-test="newRestaurantName"]')
+      .should('not.exist');
+
     cy.get('[data-test="addRestaurantButton"]')
       .click();
 
@@ -12,6 +15,9 @@ describe('adding a restaurant', () => {
 
     cy.get('[data-test="saveNewRestaurantButton"]')
       .click();
+
+    cy.get('[data-test="newRestaurantName"]')
+      .should('not.exist');
 
     cy.contains(restaurantName);
   });
--- src/RestaurantListPage.js
+++ src/RestaurantListPage.js
@@ -3,10 +3,18 @@ import NewRestaurantForm from './NewRestaurantForm';
 import RestaurantList from './RestaurantList';
 
 export default class RestaurantListPage extends Component {
-  state = { restaurantNames: [] }
+  state = {
+    restaurantNames: [],
+    showNewRestaurantForm: false,
+  }
+
+  handleShowNewRestaurantForm = () => {
+    this.setState({ showNewRestaurantForm: true });
+  }
 
   handleAddRestaurant = (newRestaurantName) => {
     this.setState(state => ({
+      showNewRestaurantForm: false,
       restaurantNames: [
         newRestaurantName,
         ...state.restaurantNames,
@@ -15,17 +23,26 @@ export default class RestaurantListPage extends Component {
   }
 
   render() {
-    const { restaurantNames } = this.state;
+    const {
+      restaurantNames,
+      showNewRestaurantForm,
+    } = this.state;
     return (
       <div>
         <button
           data-test="addRestaurantButton"
+          onClick={this.handleShowNewRestaurantForm}
         >
           Add Restaurant
         </button>
-        <NewRestaurantForm
-          onSave={this.handleAddRestaurant}
-        />
+        {
+          showNewRestaurantForm
+            ?
+            <NewRestaurantForm
+              onSave={this.handleAddRestaurant}
+            />
+            : null
+        }
         <RestaurantList restaurantNames={restaurantNames} />
       </div>
     );
