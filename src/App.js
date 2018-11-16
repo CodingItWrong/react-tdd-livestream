d219d6680bb1912e8c715cc539651ea4f26fb353
Josh Justice <me@codingitwrong.com>
2018-11-16 14:34:14 -0500

    Refactor to render app through React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import RestaurantListPage from './RestaurantListPage';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import {
   Col,
   Row,
 } from 'react-materialize';
+import RestaurantListPage from './RestaurantListPage';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Row>
-        <Col s={12} m={10} l={8} offset="m1 l2">
-          <RestaurantListPage />
-        </Col>
-      </Row>
+      <Router>
+        <Row>
+          <Col s={12} m={10} l={8} offset="m1 l2">
+            <Route path="/" exact component={RestaurantListPage} />
+          </Col>
+        </Row>
+      </Router>
     );
   }
 }
