284724feb1d295f370fa48b3751c8d1f72dfdb4c
Josh Justice <me@codingitwrong.com>
2018-11-18 17:12:12 -0500

    Fix restaurant e2e test

--- cypress/integration/adding_a_restaurant.spec.js
+++ cypress/integration/adding_a_restaurant.spec.js
@@ -1,15 +1,38 @@
 describe("adding a restaurant", () => {
   it("displays the restaurant in the list", () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: '/restaurants',
+      response: {
+        data: [
+          {
+            type: 'restaurants',
+            id: '1',
+            attributes: {
+              name: 'Burger Place',
+            },
+          },
+        ],
+      },
+    });
+
+    const serverRestaurantName = "Burger Place";
     const restaurantName = "Sushi Place";
 
     cy.visit("http://localhost:1234");
 
+    shouldLoadRestaurantsFromServer(serverRestaurantName);
     modalNotShownAtTheStart();
     modalCanBeCancelled();
     modalDisplaysValidationErrors();
     modalClearsOutValidationErrorsWhenClosed();
     modalAllowsAddingRestaurant(restaurantName);
   });
+
+  function shouldLoadRestaurantsFromServer(restaurantName) {
+    cy.contains(restaurantName);
+  }
 
   function modalNotShownAtTheStart() {
     cy.get('[data-testid="newRestaurantName"]').should("not.be.visible");
